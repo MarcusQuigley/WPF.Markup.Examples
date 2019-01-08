c00f32c747ca82b8127ebdac032766f0387ec2ea
[MarcusQ] <marcusquigley@gmail.com>
2019-01-07 19:21:55 -0500

    Cleaning up EnvironmentExtension

--- WPF.MarkupExtensions/EnvironmentExtension.cs
+++ WPF.MarkupExtensions/EnvironmentExtension.cs
@@ -7,17 +7,22 @@ using System.Windows.Markup;
 
 namespace WPF.MarkupExtensions
 {
-    class EnvironmentExtension : MarkupExtension
+    [MarkupExtensionReturnType(typeof(string))]
+    public class EnvironmentExtension : MarkupExtension
     {
         public string VariableName { get; set; }
 
-        public EnvironmentExtension(string variableName)
-        {
-            VariableName = variableName;
-        }
+        //public EnvironmentExtension(string variableName)
+        //{
+        //    VariableName = variableName;
+        //}
 
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
+            if (string.IsNullOrEmpty(VariableName))
+            {
+                throw new ArgumentException("Variable name can't be empty");
+            }
             return Environment.GetEnvironmentVariable(VariableName);
         }
     }

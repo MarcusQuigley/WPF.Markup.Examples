3bcd24cfb56aa5214cd50b16c11b64a97daa4b9d
[MarcusQ] <marcusquigley@gmail.com>
2019-01-07 19:15:12 -0500

    Adding EnvironmentExtension

---
+++ WPF.MarkupExtensions/EnvironmentExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace WPF.MarkupExtensions
+{
+    class EnvironmentExtension : MarkupExtension
+    {
+        public string VariableName { get; set; }
+
+        public EnvironmentExtension(string variableName)
+        {
+            VariableName = variableName;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Environment.GetEnvironmentVariable(VariableName);
+        }
+    }
+}

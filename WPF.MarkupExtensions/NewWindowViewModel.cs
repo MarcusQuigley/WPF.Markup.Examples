cdb55dbd4d4a5b9554457ab151dbd1d2b9f70176
[MarcusQ] <marcusquigley@gmail.com>
2019-01-11 15:25:50 -0500

    COmmand binding

--- WPF.MarkupExtensions/NewWindowViewModel.cs
+++ WPF.MarkupExtensions/NewWindowViewModel.cs
@@ -4,20 +4,30 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace WPF.MarkupExtensions
 {
     class NewWindowViewModel : INotifyPropertyChanged
     {
         TimeSpan currentTime = DateTime.Now.TimeOfDay;
+        string _dayString = string.Empty;
+        ICommand _dayCommand;
         public event PropertyChangedEventHandler PropertyChanged;
 
+
+        public NewWindowViewModel()
+        {
+            _dayCommand = new RelayCommand(GetDay);
+        }
         protected void OnPropertyChanged(string p)
         {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(p));
         }
 
+        public ICommand DayCommand   => _dayCommand;
+ 
         public TimeSpan CurrentTime
         {
             get => currentTime;
@@ -30,5 +40,21 @@ namespace WPF.MarkupExtensions
                 }
             }
         }
+
+        public string DayString { get => _dayString;
+                set {
+                if (_dayString != value)
+                {
+                    _dayString = value;
+                    OnPropertyChanged("DayString");
+                }
+            } }
+
+        void GetDay(object o)
+        {
+            Random r = new Random();
+            DayString = Enum.GetName(typeof(DayOfWeek), r.Next(7));
+        }
+
     }
 }
---
+++ WPF.MarkupExtensions/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPF.MarkupExtensions
+{
+    public class RelayCommand : ICommand
+    {
+        Action<object> _execute;
+        Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        { }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return (_canExecute != null) ? _canExecute(parameter) : true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}

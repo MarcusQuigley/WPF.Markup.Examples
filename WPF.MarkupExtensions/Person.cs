1630d1a7710de83dbfa52c65dff9c9c866f46b71
[MarcusQ] <marcusquigley@gmail.com>
2019-01-14 13:22:25 -0500

    Adding AsyncObservableCollection

---
+++ WPF.MarkupExtensions/Person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.MarkupExtensions
+{
+    class Person : INotifyPropertyChanged
+    {
+        int id;
+        string name;
+        string city;
+
+        public int Id
+        {
+            get => id;
+            set
+            {
+                if (id != value)
+                {
+                    id = value;
+                    OnPropertyChanged("Id");
+                }
+            }
+        }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+        public string City
+        {
+            get => city;
+            set
+            {
+                if (city != value)
+                {
+                    city = value;
+                    OnPropertyChanged("City");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+    }
+}
